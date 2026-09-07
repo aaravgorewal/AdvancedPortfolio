0c5af69cc8993db867f4ea6a27ad9ce8e478d2a8
Aarav Saini <sainiaarav331@gmail.com>
2026-09-07 23:18:46 +0530

    style: remove redundant comment separator in About component layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -121,7 +121,7 @@ export const About = () => {
 
           </div>
 
-          {/* ========================================= */}
+
           {/* RIGHT COLUMN (Approx 35%) */}
           {/* ========================================= */}
           <div className="lg:w-[35%] flex flex-col justify-start lg:border-l lg:border-[#EDE7DC]/10 lg:pl-12 xl:pl-16">
