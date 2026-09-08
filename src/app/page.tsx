2a6824a0d7b64e3d66ee89c94086b0ec52cbabbf
Aarav Saini <sainiaarav331@gmail.com>
2026-09-08 22:02:54 +0530

    refactor: remove redundant hero portal comment from page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { Achievements } from "@/components/Achievements";
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
-      {/* 1. Hero Portal stage */}
+
       <Hero />
 
       {/* 2. Philosophy statement fold */}
